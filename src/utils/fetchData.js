5a2df94483b89240dcb631b600c5904787d751fb
Christopher Godstime <christophergodstime45@gmail.com>
2024-02-12 17:33:51 +0100

    public news post

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,38 +1,39 @@
 import axios from "axios";
+const backend = "https://actlip.onrender.com";
 
 export const getDataApi = async (url) => {
-  const res = await axios.get(`/api/${url}`);
+  const res = await axios.get(`${backend}/api/${url}`);
   return res;
 };
 
 export const getNewsApi = async (url) => {
-  const res = await axios.get(`/api/${url}`);
+  const res = await axios.get(`${backend}/api/${url}`);
   return res;
 };
 
 export const postDataApi = async (url, post, token) => {
-  const res = await axios.post(`/api/${url}`, post, {
+  const res = await axios.post(`${backend}/api/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
 };
 
 export const putDataApi = async (url, post, token) => {
-  const res = await axios.put(`/api/${url}`, post, {
+  const res = await axios.put(`${backend}/api/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
 };
 
 export const patchDataApi = async (url, post, token) => {
-  const res = await axios.patch(`/api/${url}`, post, {
+  const res = await axios.patch(`${backend}/api/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
 };
 
 export const deleteDataApi = async (url, token) => {
-  const res = await axios.delete(`/api/${url}`, {
+  const res = await axios.delete(`${backend}/api/${url}`, {
     headers: { Authorization: token },
   });
   return res;
