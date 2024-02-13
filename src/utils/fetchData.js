e3d84b861c7de5ae3830d8db2603309ddba87642
Christopher Godstime <christophergodstime45@gmail.com>
2024-02-13 07:51:17 +0100

    refresh token

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,24 +2,24 @@ import axios from "axios";
 const backend = process.env.REACT_APP_BACKEND_URL;
 
 export const getDataApi = async (url) => {
-  const res = await axios.get(`${backend}/api/${url}`);
+  const res = await axios.get(`https://actlip.onrender.com/api/${url}`);
   return res;
 };
 
 export const getNewsApi = async (url) => {
-  const res = await axios.get(`${backend}/api/${url}`);
+  const res = await axios.get(`https://actlip.onrender.com/api/${url}`);
   return res;
 };
 
 export const postDataApi = async (url, post, token) => {
-  const res = await axios.post(`${backend}/api/${url}`, post, {
+  const res = await axios.post(`https://actlip.onrender.com/api/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
 };
 
 export const putDataApi = async (url, post, token) => {
-  const res = await axios.put(`${backend}/api/${url}`, post, {
+  const res = await axios.put(`https://actlip.onrender.com/api/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
